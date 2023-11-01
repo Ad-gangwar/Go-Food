912d526ecfc6c24e9fef033b48e27a3cdf19c479
Ad-gangwar <adgangwar54@gmail.com>
2023-11-01 11:41:22 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fetchData = require('./db');
+require('dotenv').config();
+const PORT=process.env.PORT;
+const URL=process.env.BASE_URL;
 const app = express();
 
 app.use((req, res, next)=>{
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", URL);
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
@@ -23,6 +26,6 @@ app.use('/api', require('./Routes/createUser'));
 app.use('/api', require('./Routes/displayData'));
 app.use('/api', require('./Routes/OrderData'));
 
-app.listen("5000", () => {
-    console.log("Server started at port 5000");
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
 });
