256d6ec81e05a4412f7a8d27d95d4b1082f6f898
Ad-gangwar <adgangwar54@gmail.com>
2023-11-02 20:26:27 +0530

    added again

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fetchData = require('./db');
+require('dotenv').config();
+const PORT=process.env.PORT || "5000";
+const URL=process.env.BASE_URL;
+const app = express();
+console.log(URL)
+
+app.use((req, res, next)=>{
+    res.setHeader("Access-Control-Allow-Origin", URL);
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
+// Middleware to parse JSON requests
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+// Assuming 'createUser' is a router module
+app.use('/api', require('./Routes/createUser'));
+app.use('/api', require('./Routes/displayData'));
+app.use('/api', require('./Routes/OrderData'));
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
