1de78680676bd90ee8357b69dfe384d88a08e105
Ad-gangwar <adgangwar54@gmail.com>
2023-12-23 01:16:33 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const fetchData = require('./db');
 require('dotenv').config();
 const PORT=process.env.PORT || "5000";
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use((req, res, next)=>{
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
@@ -15,6 +17,7 @@ app.use((req, res, next)=>{
 })
 // Middleware to parse JSON requests
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
     res.send("hello");
